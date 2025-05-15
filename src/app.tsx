940e2f3829adb94f55741a187d8f16074b3374ff
aljary12 <ryadisp@gmail.com>
2025-05-15 23:20:42 +0800

    feat: add redux persist and async storage

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 import Routes from './navigator/root-navigator';
 import {Provider} from 'react-redux';
-import {store} from './stores';
+import {persistor, store} from './stores';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {PersistGate} from 'redux-persist/integration/react';
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <Routes />
-      </SafeAreaProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <Routes />
+        </SafeAreaProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,15 +1,45 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import searchReducer from './search-slice';
 import placeReducer from './place-slice';
 import historyReducer from './history-slice';
 
-export const store = configureStore({
-  reducer: {
-    history: historyReducer,
-    search: searchReducer,
-    place: placeReducer,
-  },
+const rootReducer = combineReducers({
+  history: historyReducer,
+  search: searchReducer,
+  place: placeReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['history'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
